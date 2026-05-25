13200d22a34f73d000ad0101fd94021fa7c36fb1
peppo2026 <peppo2026@users.noreply.github.com>
2026-05-25 20:17:29 +0300

    settings ayarlarini env dosyasindan oku

--- proje/settings.py
+++ proje/settings.py
@@ -22,12 +22,13 @@ load_dotenv(BASE_DIR / ".env")
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_jr7fk-h_rcs_1(_$2m1a50jey$l)0*!6t@yq0pu(upst_mmgu'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "local-dev-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.254.74","172.20.10.2", "localhost", "127.0.0.1"]
+DEBUG = os.environ.get("DJANGO_DEBUG", "False") == "True"
+
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 
 # Application definition
