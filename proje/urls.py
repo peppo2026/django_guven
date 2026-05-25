749ecc0024ab55576cffdb79ba309fda80d250c9
peppo2026 <peppo2026@users.noreply.github.com>
2026-05-26 01:51:02 +0300

    admin yolunu reveber guven yap

--- proje/urls.py
+++ proje/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('reveber-guven/', admin.site.urls),
 
     path(
         "sifremi-unuttum/",
